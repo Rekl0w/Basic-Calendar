ae3ece1b52c88566934ce0af3c179afcc827071a
musabeytekin <02190201003@ogr.inonu.edu.tr>
2022-12-16 17:44:26 +0300

    some functions added

--- Calendar.py
+++ Calendar.py
@@ -2,5 +2,17 @@ import calendar
    
 yy = int(input("Enter year: "))  
 mm = int(input("Enter month: "))  
-   
-print(calendar.month(yy, mm))
+
+def print_full_calendar_from_year(year):
+    print("Full Calendar")
+    print(calendar.calendar(year))
+
+def print_month_calendar_from_year(year, month):
+    print("Month Calendar")
+    print(calendar.month(year, month))
+
+def print_month_calendar_from_year_and_weekday(year, month):
+    print("Month Calendar with Weekday")
+    print(calendar.monthcalendar(year, month))
+
+
